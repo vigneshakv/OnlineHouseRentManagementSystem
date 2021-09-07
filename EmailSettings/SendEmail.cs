faee02834e06c4f3b967c14a058e04a2922d668e
Vigneshwaran <vigneshwaran.akv@gmail.com>
2021-09-07 09:10:18 +0530

    try block added to email

--- EmailSettings/SendEmail.cs
+++ EmailSettings/SendEmail.cs
@@ -10,18 +10,25 @@ namespace OnlineHouseRentManagementSystem.EmailSettings
     {
         public static void Send(string email,string emailBody)
         {
-            MailMessage mailMessage = new MailMessage("vigneshwaranmicrosoft.akv@gmail.com", email);
-            mailMessage.Subject = "Property Alert";
-            mailMessage.Body = emailBody;
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
-            smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new System.Net.NetworkCredential()
+            try
             {
-                UserName = "vigneshwaranmicrosoft.akv@gmail.com",
-                Password = "Letsrocky!!"
-            };
-            smtpClient.EnableSsl = true;
-            smtpClient.Send(mailMessage);
+                MailMessage mailMessage = new MailMessage("vigneshwaranmicrosoft.akv@gmail.com", email);
+                mailMessage.Subject = "Property Alert";
+                mailMessage.Body = emailBody;
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential()
+                {
+                    UserName = "vigneshwaranmicrosoft.akv@gmail.com",
+                    Password = "Letsrocky!!"
+                };
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mailMessage);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
